6547bd5579febbb5d969d95f71136fc73109c4f7
KuaterCraft <kuatercraft@gmail.com>
2022-09-20 14:40:51 +0530

    Update Broadcast.php

--- src/KuaterCraft/MCDC/Broadcast.php
+++ src/KuaterCraft/MCDC/Broadcast.php
@@ -23,33 +23,6 @@ class Broadcast extends Task {
             "content"    => $msg,
             "username"	=> $this->main->getDiscordWebHookName()
         ];
-
-        if(is_array($options) && count($options) > 0) {
-            $embed = Array();
-            if(array_key_exists("title", $options)) {
-                
-                $embed["title"] = $options["title"];
-            }
-
-            if(array_key_exists("description", $options)) {
-                $embed["description"] = $options["description"];
-            }
-
-            if(array_key_exists("color", $options)) {
-                $embed["color"] = $options["color"];
-            }
-
-            if(array_key_exists("footer", $options)) {
-                $embed["footer"] = array("text" => $options["footer"]);
-            }
-            
-            if(array_key_exists("enable_pings", $options) && $options["enable_pings"] !== true) {
-              $curlopts["allowed_mentions"]["parse"] = Array();
-            }
-
-            $curlopts["embed"] = $embed;
-        }
-        
         $this->main->getServer()->getAsyncPool()->submitTask(new SendAsync($player, $this->main->getDiscordWebHookURL(), serialize($curlopts)));
     }
 }
